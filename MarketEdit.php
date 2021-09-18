3014a60d3e77657017686a141d62946f1ffbfbed
dristo-pixel <juliusriprapnamata@gmail.com>
2021-09-18 15:02:06 +0800

    Market Edit Design

--- MarketEdit.php
+++ MarketEdit.php
@@ -126,17 +126,47 @@
 <head>
 
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
 	<link rel="stylesheet" type="text/css" href="MarketEditCSS.css">
 	<title><?php echo $_SESSION["market"]?> Edit</title>
 
 </head>	
 <body>
-	<h4>Insert "EVailable Goods header here (same nung sa Research.php)"</h4>
-	<h1>Edit <?php echo $_SESSION["market"]?></h1>
-	<div id = "product_table">
+	<header>
+		<nav>
+			<ul class="links">
+				<li><a href="Research.php">Buy</a></li>
+				<li><a href="Talk.php">Talk</a></li>
+				<li><a href="Edit.php">Edit</a></li>
+				<li><a href="Suggest.php">Suggest</a></li>
+				<li><a href="About.php">About</a></li>
+			</ul>
+		</nav>
+		<a href = "Research.php" class = "evg">E-Vailable Goods</a>
+		<ul>
+		<li class = "dropdown"><a href = "Profile.php" class="pic">
+			<div class="prof"><img src = "<?php echo $_SESSION["prof_pic"]?>" alt = "Avatar" class = "dp">
+			</div>
+		</a>
+		<div class="dlinks">
+      			<a href="Profile.php">Profile</a>
+      			<a href="#">Help & Support</a>
+      			<a href="Logout.php">Logout</a>
+    	</div>
+    	</li>
+		</ul>
+
+	</header>
+	<div class = "buyp">
+	<h1 id = "buy">Edit <?php echo $_SESSION["market"]?></h1>
+	<hr id = "fr">
+	</div>
+	<div class = "product_table">
 		<div class = "pname">
 			<h3>Product Name</h3>
-			<hr>
+			<hr id ="fhr">
 			<?php
 				$res = $conn -> query($select);
 				while($row = $res->fetch_assoc()) {
@@ -148,7 +178,7 @@
 
 		<div class = "quantity">
 			<h3>Quantity</h3>
-			<hr>
+			<hr id ="fhr">
 			<?php
 				$res = $conn -> query($select);
 				while($row = $res->fetch_assoc()) {
@@ -160,7 +190,7 @@
 
 		<div class = "unit">
 			<h3>Unit</h3>
-			<hr>
+			<hr id ="fhr">
 			<?php
 				$res = $conn -> query($select);
 				while($row = $res->fetch_assoc()) {
@@ -172,7 +202,7 @@
 
 		<div class = "price">
 			<h3>Price</h3>
-			<hr>
+			<hr id ="fhr">
 			<?php
 				$res = $conn -> query($select);
 				while($row = $res->fetch_assoc()) {
@@ -183,7 +213,7 @@
 		</div>
 		<div class = "editp">
 			<h3>Edit Quantity</h3>
-			<hr>
+			<hr id ="fhr">
 			<form method = post action = <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
 				<?php
 					$i  = 0;
@@ -192,12 +222,12 @@
 						$idname1 = "num1".strval($i);
 						$temp_var1 = "a".$row["productname"];
 						echo "<div class = filler>\n\t\t\t\t\t"."<input id = $idname1 type = number name = $temp_var1 value = $row[quantity] min = 0>
-						<button type = button onclick = \"
+						<button id = minus type = button onclick = \"
 							function dec(){
 								document.getElementById('$idname1').stepDown();
 							} 
 							dec();\" >-</button>
-						<button type = button onclick = \"
+						<button id = plus type = button onclick = \"
 							function inc(){
 								document.getElementById('$idname1').stepUp();
 							} 
@@ -211,7 +241,7 @@
 		</div>
 		<div class = "editq">
 			<h3>Edit Price</h3>
-			<hr>
+			<hr id ="fhr">
 				<?php
 					$i  = 0;
 					$res = $conn -> query($select);
@@ -219,12 +249,12 @@
 						$idname2 = "num2".strval($i);
 						$temp_var2 = "b".$row["productname"];
 						echo "<div class = filler>\n\t\t\t\t\t"."<input id = $idname2 type = number name = $temp_var2 value = $row[price] min = 0>
-						<button type = button onclick = \"
+						<button id = minus type = button onclick = \"
 							function dec(){
 								document.getElementById('$idname2').stepDown();
 							} 
 							dec();\" >-</button>
-						<button type = button onclick = \"
+						<button id = plus type = button onclick = \"
 							function inc(){
 								document.getElementById('$idname2').stepUp();
 							} 
@@ -237,23 +267,16 @@
 				?>
 				
 		</div>
-		<div id = "float_form" style = "display: none;">
-			Product Name: <input id = "prodname" type = "text" name = "prodname" value = <?php echo $prodname;?> > <span class = "error">* <?php echo $err;?> </span> <br>
-			Quantity:  <input id = "quan" type = "number" name = "quan" min = 0 value = <?php echo $quan;?>> <span class = "error">* <?php echo $err;?> </span> <br>
-			Unit:  <input id = "unit" type = "text" name = "unit" value = <?php echo $unit;?>> <span class = "error">* <?php echo $err;?> </span> <br>
-			Price:  <input id = "price" type = "number" name = "price" min = 0 value = <?php echo $price;?>> <span class = "error">* <?php echo $err;?> </span> <br>
-			<!--
-					Kailangang nakafix position netong Save sa lower right ng page 
-				-->
+		<div id = "float_form" style = "display: block;">
+			<label id = "prodname">Product Name:</label> <input id = "prodname" type = "text" name = "prodname" value = <?php echo $prodname;?> > <span class = "error">* <?php echo $err;?> </span> <br>
+			<label id = "quan">Quantity:</label> <input id = "quan" type = "number" name = "quan" min = 0 value = <?php echo $quan;?>> <span class = "error">* <?php echo $err;?> </span> <br>
+			<label id = "unit">Unit:</label> <input id = "unit" type = "text" name = "unit" value = <?php echo $unit;?>> <span class = "error">* <?php echo $err;?> </span> <br>
+			<label id = "price">Price:</label>  <input id = "price" type = "number" name = "price" min = 0 value = <?php echo $price;?>> <span class = "error">* <?php echo $err;?> </span> <br>
 			
 		</div>
-		<input type = "submit" value = "Save" id = "save">
+		<input type = "submit" value = "Save" class = "save">
 		</form>
 	</div>
-	<button type = "button" onclick="magic()" id = "show">Add Products</button>
-	<!--
-		Insert nav sht here, yung may Buy Edit Talk (same sa lahat ng page)
-	-->
-
+	<button type = "button" onclick="magic()" class = "show">Add Products</button>
 </body>
 </html>
